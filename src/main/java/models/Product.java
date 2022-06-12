065b86a47986713901c06e0cc816f6ece73c068a
Karen Frazão <karen.pfr00@gmail.com>
2022-06-11 21:50:48 -0300

    Criaçao do Sellerapp

---
+++ src/main/java/models/Product.java
@@ -0,0 +1,35 @@
+package models;
+
+public class Product {
+    private String id;
+    private String descricao;
+    private double preco;
+
+    public Product(String id, String descricao, double preco) {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
